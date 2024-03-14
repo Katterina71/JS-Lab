1c89ae351a00ad492beccaf4160970b49a2e3162
Kate Iliu <beviatori@gmail.com>
2024-03-14 17:11:25 -0600

    Complete LAB1 JS

--- LAB1/lab1.js
+++ LAB1/lab1.js
@@ -1,69 +1,74 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
 
-// Part 2
-
-// You are planning a cross-country road trip!
-// The distance of the trip, in total, is 1,500 miles.
-// Your car’s fuel efficiency is as follows:
-// At 55 miles per hour, you get 30 miles per gallon.
-// At 60 miles per hour, you get 28 miles per gallon.
-// At 75 miles per hour, you get 23 miles per gallon.
-// You have a fuel budget of $175.
-// The average cost of fuel is $3 per gallon.
-// Set up a program to answer the following questions:
-// How many gallons of fuel will you need for the entire trip?
-// Will your budget be enough to cover the fuel expense?
-// How long will the trip take, in hours?
-
-//Create variable declarations
-const tripdistanse = 1500; 
-
-const mpg55 = 30;
-const mpg60 = 28;
-const mpg75 = 23;
-
-const fuelcost = 3;
-const budget = 175; 
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
 
-// Calculate how much gasoline is required for each speed
-const galused55mph = tripdistanse /mpg55; // 55 m/h
-const galused60mph = tripdistanse /mpg60; // 60 m/h
-const galused75mph = tripdistanse /mpg75; // 75 m.h
-
-// Check - Calculate how much gasoline is required for each speed
-console.log(Math.round(galused55mph) + " Gallons. Speed 55 miles per hour");
-console.log(Math.round(galused60mph)+ " Gallons. Speed 60 miles per hour");
-console.log(Math.round(galused75mph) + " Gallons. Speed 75 miles per hour");
-
-// Spend money for trip depend on speed 
-const budget55mph = galused55mph * fuelcost; // 55 m/h
-const budget60mph = galused60mph * fuelcost; // 60 m/h
-const budget75mph = galused75mph * fuelcost; // 75 m.h
-
-// Check - Spend money for trip depend on speed
-console.log(Math.round(budget55mph)+ "$ Budget. Speed 55 miles per hour");
-console.log(Math.round(budget60mph)+ "$ Budget. Speed 60 miles per hour");
-console.log(Math.round(budget75mph)+ "$ Budget. Speed 75 miles per hour");
-
-//How long will the trip take, in hours
-let hourspertrip55pmh = tripdistanse / 55;
-let  hourspertrip60pmh = tripdistanse / 60;
-let  hourspertrip75pmh = tripdistanse /75;
-
-// Check - How long will the trip take, in hours
-console.log(Math.round(hourspertrip55pmh)+ " Hours. Speed 55 miles per hour");
-console.log(Math.round(hourspertrip60pmh)+ " Hours. Speed 60 miles per hour");
-console.log(Math.round(hourspertrip75pmh)+  " Hours. Speed 75 miles per hour");
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 
 
-// Find the best speed solution for the trip
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
-if (budget55mph <= budget && hourspertrip55pmh < hourspertrip60pmh && hourspertrip55pmh < hourspertrip75pmh) {
-  console.log("The best speed is 55 m/h. It will take "+ Math.round(hourspertrip55pmh) + " hours and you spend " + Math.round(galused55mph) + " Gallons. Budget will be "+ Math.round(budget55mph) + "$." ); 
-} else if (budget60mph <= budget && hourspertrip60pmh < hourspertrip55pmh) {
-  console.log("The best speed is 60 m/h. It will take "+ Math.round(hourspertrip60pmh) + " hours and you spend " + Math.round(galused60mph) + " Gallons. Budget will be "+ Math.round(budget60mph) + "$." ); 
-}
-else {
-  console.log("The best speed is 75 m/h. It will take "+ Math.round(hourspertrip75pmh) + " hours and you spend " + Math.round(galused75mph) + " Gallons. Budget will be "+ Math.round(budget75mph) + "$." );
-}
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+
+
+// Here's another example of how this COULD be done,
+// but it SHOULD NOT be done this way. As programmers,
+// we break things into small, manageable pieces so that
+// they can be better understood, scaled, and maintained.
+const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
+  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
+  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+// Implement the following:
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const divisible5 = (n1 + n2 + n3 + n4) / 5;
+console.log(divisible5);
+
+// Check if the first number is larger than the last. Cache the result in a variable.
+const comparefirstlast = n1 > n4;
+console.log(comparefirstlast);
+
+// Accomplish the following arithmetic chain:
+
+// Subtract the first number from the second number.
+const subtractfirstsecont = n2 - n1; 
+
+// Multiply the result by the third number.
+const multiplybythird = subtractfirstsecont * n3; 
+
+// Find the remainder of dividing the result by the fourth number.
+const remainder = multiplybythird % n4; 
+
+// Finally, log the results.
+console.log(remainder);
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
+console.log(isValid);
